a49f04cd0410bd5101d27995f92a821e196b47af
chris <chris@ctfDebian.ctfDebian.local>
2025-03-16 17:31:26 -0500

    added comments to seen_guessing

--- seen_guessing/exp.py
+++ seen_guessing/exp.py
@@ -28,3 +28,16 @@ p.sendline(b'Samanu')
 p.sendline(b'Serkeh')
 
 p.interactive()
+
+
+'''
+This one was relatively easy to see where the overflow initially
+was.  The difficulty is the binary purposefully puts you in an
+infinite loop until you guess all of the 7 names seen here.  I
+found these pretty easily in Ghidra, the binary was not stripped
+so I just clicked on the array "Seen" and it showed them all.
+Finally, I realized I had to just make the 7 correct guesses and
+then the function would exit.  At that point RIP is overwritten
+so I just put in the address of the win variable and bobs your
+uncle as they say.
+'''
